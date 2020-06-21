29c2d1e00f64c86006da693a210617ee9f588d1f
tamaasrory <tamaasrory@gmail.com>
2020-06-21 08:59:49 +0700

    update Styles.js

--- src/components/Styles.js
+++ src/components/Styles.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
     paddingVertical: 0,
   },
   textSecondary: {
-    fontSize: 14,
+    fontSize: 12,
     lineHeight: 20,
     color: theme.colors.backdrop,
   },
