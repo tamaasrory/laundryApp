062072ef1d1ef0052f18b279bc86a7c4c3f0aeda
tamaasrory <tamaasrory@gmail.com>
2020-06-22 23:10:25 +0700

    update - static color for toolbar and status bar

--- src/core/theme.js
+++ src/core/theme.js
@@ -9,13 +9,14 @@ export const theme = {
     secondary: '#414757',
     error: '#f13a59',
     accent: '#f13a59',
-    tabHome: '#0086ff', // '#0086ff',
-    tabHomeStatusBar: '#007aea', //'#0071e2'
-    tabOrder: '#0086ff', // '#1dbc60',
-    tabOrderStatusBar: '#007aea', //'#1bae59'
-    tabHistory: '#0086ff', // '#7d8397',
-    tabHistoryStatusBar: '#007aea', //'#6d7386'
-    tabAccount: '#0086ff', // '#ff9e00',
-    tabAccountStatusBar: '#007aea', //'#ed9100'
+    tabHome: /*'#0086ff', */ '#0086ff',
+    tabHomeStatusBar: /*'#007aea', */ '#0086ff',
+    tabOrder: '#fff', // '#0086ff', // '#1a8945',
+    tabOrderStatusBar: '#fff', // '#0071e2', // '#188944',
+    tabHistory: /*'#0086ff', */ '#7d8397',
+    tabHistoryStatusBar: /*'#007aea', */ '#6d7386',
+    tabAccount: /*'#0086ff', */ '#fff',
+    tabAccountStatusBar: /*'#007aea', */ '#fff',
+    textToolBar: '#000',
   },
 };
