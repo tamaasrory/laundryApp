21b317b5742e3b00ef62a139fe5bef3c3767652f
tamaasrory <tamaasrory@gmail.com>
2020-06-28 06:36:55 +0700

    update Path to server
    Took 18 seconds

--- src/router/Path.js
+++ src/router/Path.js
@@ -2,4 +2,6 @@ export default class Path {
   static baseUrl = 'https://api.ethanlaundry.com';
   static priceListImage = `${Path.baseUrl}/img/price-list`;
   static priceListThumbImage = `${Path.baseUrl}/img/price-list/thumbnail`;
+  static BannerImage = `${Path.baseUrl}/img/banner`;
+  static BannerThumbImage = `${Path.baseUrl}/img/banner/thumbnail`;
 }
