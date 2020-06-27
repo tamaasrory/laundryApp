8c339fbe4d02a053c5bedbcbeecf8e144d9a9486
tamaasrory <tamaasrory@gmail.com>
2020-06-28 06:36:19 +0700

    add SplashScreen.js
    Took 3 hours 0 minutes

---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,32 @@
+import React, {memo} from 'react';
+import CenterContainer from '../components/CenterContainer';
+import Logo from '../components/Logo';
+import SInfo from 'react-native-sensitive-info';
+
+const SplashScreen = ({navigation}) => {
+  SInfo.getItem('token', {}).then(value => {
+    setTimeout(() => {
+      if (value) {
+        navigation.reset({
+          index: 0,
+          routes: [{name: 'MainScreen'}],
+        });
+      } else {
+        navigation.reset({
+          index: 0,
+          routes: [{name: 'LoginScreen'}],
+        });
+      }
+    }, 2000);
+  });
+
+  console.info('#render : ', 'SplashScreen.js');
+
+  return (
+    <CenterContainer>
+      <Logo />
+    </CenterContainer>
+  );
+};
+
+export default memo(SplashScreen);
